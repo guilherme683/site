9311566219657c5854a54700e20fda83e0f33a11
guilherme683 <guilherme683@hotmail.com>
2018-02-05 19:20:55 -0200

    postando pasta meat

---
+++ meat-app-starter/src/app/app.api.ts
@@ -0,0 +1,2 @@
+// Ip auterado para acesso remoto ao servidor db.json
+export const MEAT_API = 'http://172.17.4.108:3000'
---
+++ meat-app-starter/src/app/restaurants/restaurant/restaurant.model.ts
@@ -0,0 +1,9 @@
+export interface Restaurant {
+	
+	id: string
+	name: string
+	category: string
+	deliveryEstimative: string
+	rating: number
+	imagePath: string
+}
